14a1376fc6d52c5f76af0f61cc30e59c80f94d83
Aurelia <aurela87@o2.pl>
2018-07-02 20:31:22 +0200

    create PizzaBuilder

--- src/main/java/PizzaBuilder.java
+++ src/main/java/PizzaBuilder.java
@@ -1,9 +1,13 @@
 public abstract class PizzaBuilder {
+
     protected Pizza pizza;
+
+    public PizzaBuilder(){
+        pizza = new Pizza();
+    }
 
     public abstract void buildDough();
     public abstract void buildSauce();
     public abstract void buildTopping();
-
 
 }

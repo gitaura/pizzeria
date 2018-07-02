33662ba71c5a0763b92f1cee41af859132f6f849
Aurelia <aurela87@o2.pl>
2018-07-02 20:10:26 +0200

    add pizza class

---
+++ src/main/java/Pizza.java
@@ -0,0 +1,17 @@
+public class Pizza {
+   private String dough;
+    private String topping;
+    private String sauce;
+
+    public void setDought(String dought) {
+        this.dough = dought;
+    }
+
+    public void setTopping(String topping) {
+        this.topping = topping;
+    }
+
+    public void setSauce(String sauce) {
+        this.sauce = sauce;
+    }
+}
